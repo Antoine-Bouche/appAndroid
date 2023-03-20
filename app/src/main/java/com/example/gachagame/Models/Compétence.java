bd2a659d429e719f656e7cc9b576b2ead9e0f15a
Xiang Luc <xiangluc01@gmail.com>
2023-03-20 14:21:16 +0100

    Ajout des accesseurs

--- app/src/main/java/com/example/gachagame/Models/Compétence.java
+++ app/src/main/java/com/example/gachagame/Models/Compétence.java
@@ -1,4 +1,14 @@
 package com.example.gachagame.Models;
 
 public class Compétence {
+
+    private String nom_comp;
+    private String type_comp;
+    private String comp_desc;
+
+    public Compétence(String nom_comp, String type_comp, String comp_desc) {
+        this.nom_comp = nom_comp;
+        this.type_comp = type_comp;
+        this.comp_desc = comp_desc;
+    }
 }
--- app/src/main/java/com/example/gachagame/Models/Joueur.java
+++ app/src/main/java/com/example/gachagame/Models/Joueur.java
@@ -1,4 +1,54 @@
 package com.example.gachagame.Models;
 
+import java.util.ArrayList;
+
 public class Joueur {
+
+    private String pseudo;
+    private String email;
+    private String mot_de_passe;
+    private ArrayList<Compétence> listeCompétence;
+
+    public Joueur(String pseudo, String email, String mot_de_passe) {
+        this.pseudo = pseudo;
+        this.email = email;
+        this.mot_de_passe = mot_de_passe;
+        this.listeCompétence = new ArrayList<>();
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMot_de_passe() {
+        return mot_de_passe;
+    }
+
+    public void setMot_de_passe(String mot_de_passe) {
+        this.mot_de_passe = mot_de_passe;
+    }
+
+    public ArrayList<Compétence> getListeCompétence() {
+        return listeCompétence;
+    }
+
+    public void setListeCompétence(ArrayList<Compétence> listeCompétence) {
+        this.listeCompétence = listeCompétence;
+    }
+
+    public void addCompétence(Compétence comp) {
+        this.listeCompétence.add(comp);
+    }
 }
