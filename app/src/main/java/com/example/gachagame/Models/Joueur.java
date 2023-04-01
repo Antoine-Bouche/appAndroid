8f1dcd085a0a6b13869299d37bb00002020fe982
Luc <xiangluc01@gmail.com>
2023-04-01 19:55:53 +0200

    variable retiré qui vont etre dans utilisateur

--- app/src/main/java/com/example/gachagame/Models/Joueur.java
+++ app/src/main/java/com/example/gachagame/Models/Joueur.java
@@ -3,14 +3,10 @@ package com.example.gachagame.Models;
 public class Joueur {
 
     private String pseudo;
-    private String email;
-    private String mot_de_passe;
     private Compétence[] listeCompétence;
 
-    public Joueur(String pseudo, String email, String mot_de_passe) {
+    public Joueur(String pseudo) {
         this.pseudo = pseudo;
-        this.email = email;
-        this.mot_de_passe = mot_de_passe;
         this.listeCompétence = new Compétence[4];
     }
 
@@ -20,22 +16,6 @@ public class Joueur {
 
     public void setPseudo(String pseudo) {
         this.pseudo = pseudo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMot_de_passe() {
-        return mot_de_passe;
-    }
-
-    public void setMot_de_passe(String mot_de_passe) {
-        this.mot_de_passe = mot_de_passe;
     }
 
     public Compétence[] getListeCompétence() {
