930eaff1da110ba3f94b2a4a40c1ffcfce8232f5
Luc <xiangluc01@gmail.com>
2023-04-01 19:56:34 +0200

    accesseurs compétence

--- app/src/main/java/com/example/gachagame/Models/Compétence.java
+++ app/src/main/java/com/example/gachagame/Models/Compétence.java
@@ -11,4 +11,28 @@ public class Compétence {
         this.type_comp = type_comp;
         this.comp_desc = comp_desc;
     }
+
+    public String getNom_comp() {
+        return nom_comp;
+    }
+
+    public void setNom_comp(String nom_comp) {
+        this.nom_comp = nom_comp;
+    }
+
+    public String getType_comp() {
+        return type_comp;
+    }
+
+    public void setType_comp(String type_comp) {
+        this.type_comp = type_comp;
+    }
+
+    public String getComp_desc() {
+        return comp_desc;
+    }
+
+    public void setComp_desc(String comp_desc) {
+        this.comp_desc = comp_desc;
+    }
 }
