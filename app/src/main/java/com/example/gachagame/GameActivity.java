14c7ef192fe456bd99a0ee26f7436952042f0ef6
Luc <xiangluc01@gmail.com>
2023-04-10 19:59:54 +0200

    Feature end

--- app/src/main/java/com/example/gachagame/GameActivity.java
+++ app/src/main/java/com/example/gachagame/GameActivity.java
@@ -1,7 +1,10 @@
 package com.example.gachagame;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -49,7 +52,7 @@ public class GameActivity extends AppCompatActivity {
         monstre_healthbar.setMax(m.getHp());
         monstre_healthbar.setProgress(m.getHp());
 
-        Joueur j = new Joueur("Ma bite",1000,10,1);
+        Joueur j = new Joueur("Ma bite",100,10,1);
 
         hero_healthbar.setMax(j.getHp());
         hero_healthbar.setProgress(j.getHp());
@@ -64,16 +67,15 @@ public class GameActivity extends AppCompatActivity {
                 monstre_healthbar.setProgress(newPV);
 
                 if (newPV <= 0) {
-                    // Si le monstre courant est mort, passer au monstre suivant ou terminer le jeu
-                    if (monsterList.getCurrentIndex() < monsterList.getMonsterList().size() - 1) {
+                    if (monsterList.getCurrentIndex() < monsterList.getNumberOfMonstres() - 1) {
                         monsterList.setCurrentIndex(monsterList.getCurrentIndex()+1);
-                        Monster nouveauMonstreCourant = monsterList.getCurrentMonstre();
-                        nom_monstre.setText(nouveauMonstreCourant.getName());
-                        monstre_healthbar.setMax(nouveauMonstreCourant.getHp());
-                        monstre_healthbar.setProgress(nouveauMonstreCourant.getHp());
+                        Monster newCurrentMonster = monsterList.getCurrentMonstre();
+                        nom_monstre.setText(newCurrentMonster.getName());
+                        monstre_healthbar.setMax(newCurrentMonster.getHp());
+                        monstre_healthbar.setProgress(newCurrentMonster.getHp());
                     } else {
-                        Toast.makeText(GameActivity.this, "Fin du jeu", Toast.LENGTH_SHORT).show();
                         handler.removeCallbacks(attaquerRunnable);
+                        endGame();
                     }
                 }
             }
@@ -95,6 +97,7 @@ public class GameActivity extends AppCompatActivity {
                 });
                 if (newPV <= 0) {
                     handler.removeCallbacks(attaquerRunnable);
+                    gameOver();
                 } else {
                     handler.postDelayed(this, 5000);
                 }
@@ -107,19 +110,51 @@ public class GameActivity extends AppCompatActivity {
     private List<Monster> initMonster() {
 
         List<Monster> list = new ArrayList<>();
-        list.add(new Monster(100,10,1,"Gobelin"));
+        list.add(new Monster(100,100,1,"Gobelin"));
         list.add(new Monster(1,1,1,"Axel"));
         list.add(new Monster(10,2,1,"Mananta"));
-        list.add(new Monster(1000,100,1,"je suis raciste"));
+        list.add(new Monster(100,100,1,"je suis raciste"));
 
         return list;
     }
 
     private void endGame() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
 
+        builder.setTitle("Game End");
+
+        builder.setMessage("Congratulations, you have defeated all demon lord monsters!");
+
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Close", (DialogInterface.OnClickListener) (dialog, which) -> {
+            Intent intent = new Intent(GameActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
+
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
     }
 
     private void gameOver() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
 
+        builder.setTitle("Game Over");
+
+        builder.setMessage("Oh no, you have been defeated by the demon lord!");
+
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Close", (DialogInterface.OnClickListener) (dialog, which) -> {
+            handler.removeCallbacks(attaquerRunnable);
+            Intent intent = new Intent(GameActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
+
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
     }
+
 }
