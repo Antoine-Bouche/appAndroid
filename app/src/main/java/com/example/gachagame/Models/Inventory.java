baaec8e66e389a8b1d00d2d03bea1429be57f2be
Luc <xiangluc01@gmail.com>
2023-04-17 16:47:32 +0200

    koukoubeh

--- app/src/main/java/com/example/gachagame/Models/Inventory.java
+++ app/src/main/java/com/example/gachagame/Models/Inventory.java
@@ -3,5 +3,5 @@ package com.example.gachagame.Models;
 import java.util.ArrayList;
 
 public class Inventory {
-    private ArrayList<Compétence> compétences;
+    private ArrayList<Competence> compétences;
 }
