372bf02eb20f20da959f3424c61f6f392073c935
Luc <xiangluc01@gmail.com>
2023-04-18 17:56:36 +0200

    tttt

--- app/src/main/java/com/example/gachagame/Adapter/MonsterAdapter.java
+++ app/src/main/java/com/example/gachagame/Adapter/MonsterAdapter.java
@@ -1,4 +1,41 @@
 package com.example.gachagame.Adapter;
 
-public class MonsterAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.example.gachagame.Models.Monster;
+
+import java.util.List;
+
+public class MonsterAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Monster> monster_list;
+
+    public MonsterAdapter(Context context, List<Monster> monster_list) {
+        this.context = context;
+        this.monster_list = monster_list;
+    }
+
+    @Override
+    public int getCount() {
+        return monster_list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return monster_list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
 }
